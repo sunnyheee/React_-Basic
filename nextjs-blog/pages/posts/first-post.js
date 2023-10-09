93525a7a2702b2582912e511b6ada24b9d3b8b30
sunnyheee <fool0116@naver.com>
2023-10-09 22:36:05 +0900

    画像最適化

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,10 +1,14 @@
 import Link from "next/Link";
+import Image from "next/image";
 
 export default function FirstPost() {
   return (
     <>
       <h1>First Post</h1>
-      <Link href="/">back</Link>
+      <Image src="/images/profile.jpg" width={400} height={400}></Image>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
     </>
   );
 }
