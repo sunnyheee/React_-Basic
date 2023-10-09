002363ee84956c5d40e96813cad60fe22de49410
sunnyheee <fool0116@naver.com>
2023-10-09 22:44:06 +0900

    nextjs head

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -4,6 +4,10 @@ import Image from "next/image";
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First post</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <h1>First Post</h1>
       <Image src="/images/profile.jpg" width={400} height={400}></Image>
       <h2>
